9f327082ff669d4d045e80f22abc2f103314914f
schofija <schofija@oregonstate.edu>
2021-05-15 22:52:35 -0700

    Added pytest and unittest functionality for palindrome.

--- palindrome.py
+++ palindrome.py
@@ -1,15 +1,15 @@
 def palindrome(s):
     s2 = s [::-1]
     if len(s) == 0:
-        print("Not a palindrome")
+        #print("Not a palindrome")
         return 0
 
     if s == s2:
-        print("***Your string is a palindrome!***")
+        #print("***Your string is a palindrome!***")
         return 1
     else:
-        print("Not a palindrome")
+        #print("Not a palindrome")
         return 0
         
-string1 = input("Please input a string. The program will then tell you if your string is a palindrome: ")   
-palindrome(string1)     
+#string1 = input("Please input a string. The program will then tell you if your string is a palindrome: ")   
+#palindrome(string1)     
--- test_palindrome.py
+++ test_palindrome.py
@@ -1,9 +1,23 @@
 import unittest
 import palindrome
 
+#pytest
+def test_NotPalindromepytest():
+    assert palindrome.palindrome("Hello world") == 0
+def test_Palindromepytest():
+    assert palindrome.palindrome("tacocat") == 1
+def test_emptypytest():
+    assert palindrome.palindrome("") == 0
+    
+#unittest
 class testPalindrome(unittest.TestCase):
-    def test_div(self):
-        self.assertEqual()
+    def test_NotPalindrome(self):
+        self.assertEqual(palindrome.palindrome("Hello world"), 0)
+    def test_Palindrome(self):
+        self.assertEqual(palindrome.palindrome("TACOCAT"), 1)
+    def test_empty(self):
+        self.assertEqual(palindrome.palindrome(""), 0)
         
-if __name__ == '__main__':
-    unittest.main()
+        
+#if __name__ == '__main__':
+#    unittest.main()
