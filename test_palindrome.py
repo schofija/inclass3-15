d00a98ba0050e426da1c48d61e11fb60c5cc4345
schofija <schofija@oregonstate.edu>
2021-05-15 23:21:08 -0700

    added comments explaining reasoning behind tests

--- test_palindrome.py
+++ test_palindrome.py
@@ -3,20 +3,20 @@ import palindrome
 
 #pytest
 def test_NotPalindromepytest():
-    assert palindrome.palindrome("Hello world") == 0
+    assert palindrome.palindrome("Hello world") == 0 #testing basic functionality of program
 def test_Palindromepytest():
-    assert palindrome.palindrome("tacocat") == 1
+    assert palindrome.palindrome("tacocat") == 1 #testing basic functionality of program
 def test_emptypytest():
-    assert palindrome.palindrome("") == 0
+    assert palindrome.palindrome("") == 0 #testing if program can recognize and empty input, which should not be considered a palindrome
     
 #unittest
 class testPalindrome(unittest.TestCase):
     def test_NotPalindrome(self):
-        self.assertEqual(palindrome.palindrome("elephant"), 0)
+        self.assertEqual(palindrome.palindrome("elephant"), 0) #testing basic functionality
     def test_Palindrome(self):
-        self.assertEqual(palindrome.palindrome("race car"), 1)
+        self.assertEqual(palindrome.palindrome("race car"), 1) #testing if program can detect palindromes with spaces in them
     def test_empty(self):
-        self.assertEqual(palindrome.palindrome("          "), 0)
+        self.assertEqual(palindrome.palindrome("          "), 0) #testing if program can detect empty inputs that are only spaces
         
         
 if __name__ == '__main__':
--- test_wordcount.py
+++ test_wordcount.py
@@ -3,17 +3,17 @@ import wordcount
 
 #pytest
 def test_wordcount_2_pytest():
-    assert wordcount.count("Hello world") == 2
+    assert wordcount.count("Hello world") == 2 #testing basic functionality
 def test_wordcount_4_pytest():
-    assert wordcount.count("this is a test                    ") == 4
+    assert wordcount.count("this is a test                    ") == 4 #testing if program still works if numerous spaces are added to the end of a string
 def test_emptypytest():
-    assert wordcount.count("") == 0
+    assert wordcount.count("") == 0 #testing if program works if input is empty
     
 #unittest
 class testwordcount(unittest.TestCase):
     def test_wordcount_2(self):
-        self.assertEqual(wordcount.count("                  Hello world"), 2)
+        self.assertEqual(wordcount.count("                  Hello world"), 2) #testing if adding spaces @ start of string changes output
     def test_wordcount_1(self):
-        self.assertEqual(wordcount.count("   Hey   "), 1)
+        self.assertEqual(wordcount.count("   Hey   "), 1) #testing if putting spaces around a single word changes word count
     def test_onlyspace(self):
-        self.assertEqual(wordcount.count("  "), 0)
+        self.assertEqual(wordcount.count("  "), 0) #testing if input of only spaces gives a proper output
