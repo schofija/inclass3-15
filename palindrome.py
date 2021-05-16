619255aa445b40d6c8b5e75b1fc6229d016cab88
schofija <schofija@oregonstate.edu>
2021-05-15 21:20:44 -0700

    Added palindrome checking ability.

--- palindrome.py
+++ palindrome.py
@@ -1,4 +1,8 @@
-string1 = input("Please input a string. The program will then tell you if your string is a palindrome:   ")
+string1 = input("Please input a string. The program will then tell you if your string is a palindrome: ")
 string2 = string1 [::-1]
 
-print(string2)
+if string1 == string2:
+    print("***Your string is a palindrome!***")
+    quit()
+else:
+    print("Not a palindrome")
