3038473d0ce301068db99963e12c934c29dfdc19
schofija <schofija@oregonstate.edu>
2021-05-15 22:36:10 -0700

    created unittest files. modified palindrome.py to contain a function that can be tested

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,14 @@
-string1 = input("Please input a string. The program will then tell you if your string is a palindrome: ")
-string2 = string1 [::-1]
+def palindrome(s):
+    s2 = s [::-1]
+    if len(s) == 0:
+        print("Not a palindrome")
+        quit()
 
-if len(string1) == 0:
-    print("Not a palindrome")
-    quit()
-
-if string1 == string2:
-    print("***Your string is a palindrome!***")
-    quit()
-else:
-    print("Not a palindrome")
+    if s == s2:
+        print("***Your string is a palindrome!***")
+        quit()
+    else:
+        print("Not a palindrome")
+        
+string1 = input("Please input a string. The program will then tell you if your string is a palindrome: ")   
+palindrome(string1)     
---
+++ test_palindrome.py
@@ -0,0 +1,9 @@
+import unittest
+import palindrome
+
+class testPalindrome(unittest.TestCase):
+    def test_div(self):
+        self.assertEqual()
+        
+if __name__ == '__main__':
+    unittest.main()
